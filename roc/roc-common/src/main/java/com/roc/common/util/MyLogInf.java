ffeb8f4ff835441815509d1295152ba5dc29bfbc
SYLARLL <SYLARLL@liuxianweimeng@163.com>
2018-02-08 17:43:34 +0800

    工具配置类

---
+++ roc/roc-common/src/main/java/com/roc/common/util/MyLogInf.java
@@ -0,0 +1,18 @@
+package com.roc.common.util;
+
+/**
+ * @Description:
+ * @author dingzhiwei jmdhappy@126.com
+ * @date 2017-07-05
+ * @version V1.0
+ */
+public abstract interface MyLogInf {
+
+    public abstract void debug(String paramString, Object[] paramArrayOfObject);
+
+    public abstract void info(String paramString, Object[] paramArrayOfObject);
+
+    public abstract void warn(String paramString, Object[] paramArrayOfObject);
+
+    public abstract void error(Throwable paramThrowable, String paramString, Object[] paramArrayOfObject);
+}
